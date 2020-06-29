2e508dd82644ba31601c7da0f0e355db030f05d8
iamjames77 <iamjames77@daum.net>
2020-06-29 23:53:36 +0900

    예외처리

--- calc.py
+++ calc.py
@@ -6,12 +6,12 @@ def application(environ, start_reponse):
     first_num = d.get('first_num', [''])[0]
     second_num = d.get('second_num',[''])[0]
     sum, mul =0, 0
-    if '' not in [first_num, second_num]:
+    try:
 	first_num, second_num = int(first_num), int(second_num)
 	sum = first_num + second_num
 	mul = first_num * second_num
-    if '' in [first_num, second_num]:
-	sum = -1
+    except ValueError:
+        sum = -1
 	mul = -1
     response_body = html % {'sum':sum, 'mul':mul}
     start_response('200 OK', [
